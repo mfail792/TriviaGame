eafde1e8a539d681ba0918762b8eb3e91c7fa543
Magus <mfail792@gmail.com>
2019-07-27 18:02:57 -0500

    updated var names with correct versions, corrected some syntax issues

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,40 +4,41 @@
 
 //appending correct values to answers and corresponding questions
 $('#start').on('click', function () {
+    $('#start').remove();
 
     for (var i = 0; i < questions.length; i++) {
         $('#main2').append('<h2>' + questions[i].question + '</h2');
 
         for (var j = 0; j < questions[i].guesses.length; j++) {
-            $("#main2").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+            $('#main2').append("<input type='radio' name='question-" + i + "' value='" + questions[i].guesses[j] + "'>" + questions[i].guesses[j])
         }
     }
-    })
+})
 
-    //wrapping all questions and answers into one object
-    var questions = [{
-        question: "What year did the Twins last win the World Series?",
-        guesses: ["1932", "1965", "1971", "1986", "1991"],
-        rightAnswer: "1991"
-    }, {
+//wrapping all questions and answers into one object
+var questions = [{
+    question: "What year did the Twins last win the World Series?",
+    guesses: ["1932", "1965", "1971", "1986", "1991"],
+    rightAnswer: "1991"
+}, {
 
-        question: "How many 'do do do's' does Baby Shark have?",
-        guesses: ["4", "5", "3", "6", "8"],
-        rightAnswer: "6"
-    }, {
+    question: "How many 'do do do's' does Baby Shark have?",
+    guesses: ["4", "5", "3", "6", "8"],
+    rightAnswer: "6"
+}, {
 
-        question: "What was the top grossing movie of the 90's?",
-        guesses: ["Jurassic Park", "Titanic", "Lion King", "Independence Day", "The Sixth Sense"],
-        rightAnswer: "Titanic"
-    }, {
+    question: "What was the top grossing movie of the 90's?",
+    guesses: ["Jurassic Park", "Titanic", "Lion King", "Independence Day", "The Sixth Sense"],
+    rightAnswer: "Titanic"
+}, {
 
-        question: "How much style do you really have if you drive a Mini-Van",
-        guesses: ["Some", "Little", "Non-Existent", "Mad Style", "I'm late for soccer"],
-        rightAnswer: "I'm late for soccer"
+    question: "How much style do you really have if you drive a Mini-Van",
+    guesses: ["Some", "Little", "Non-Existent", "Mad Style", "I'm late for soccer"],
+    rightAnswer: "I'm late for soccer"
 
-    }, {
+}, {
 
-        question: "Gary Busey has a black belt in Jujitsu",
-        guesses: ["True", "False",],
-        rightAnswer: "True"
-    }]
+    question: "Gary Busey has a black belt in Jujitsu",
+    guesses: ["True", "False",],
+    rightAnswer: "True"
+}]
