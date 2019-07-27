314b60d0f76648ef2d55863824a51cfdc31b2680
Magus <mfail792@gmail.com>
2019-07-27 16:53:08 -0500

    updated some divs, added questions and right answers to JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,35 @@
-$('#start').on('click', function() {
-    console.log("YOU CLICKED ME")
-})
+//need questions and answers stored in variables/arrays
+//need right answer defined
+
+//*QUESTIONS AND ANSWERS*
+
+//creating an object for each question that will make up questions section
+var questions = [{
+    question: "What year did the Twins last win the World Series?",
+    guesses: ["1932", "1965", "1971", "1986", "1991"],
+    rightAnswer: "1991"
+},]
+
+var questions = [{
+    question: "How many 'do do do's' does Baby Shark have?",
+    guesses: ["4", "5", "3", "6", "8"],
+    rightAnswer: "6"
+},]
+
+var questions = [{
+    question: "What was the top grossing movie of the 90's?",
+    guesses: ["Jurassic Park", "Titanic", "Lion King", "Independence Day", "The Sixth Sense"],
+    rightAnswer: "Titanic"
+},]
+
+var questions = [{
+    question: "How much style do you really have if you drive a Mini-Van",
+    guesses: ["Some", "Little", "Non-Existent", "Mad Style", "I'm late for soccer"],
+    rightAnswer: "I'm late for soccer"
+},]
+
+var questions = [{
+    question: "Gary Busey has a black belt in Jujitsu",
+    guesses: ["True", "False",],
+    rightAnswer: "True"
+},]
